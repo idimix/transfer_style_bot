55b89b85351d17d7eda22354dd685cfd46b43318
Dmitry Ivashnikov <dima.ivashnikov@gmail.com>
2020-07-03 01:55:17 +0300

    add MSG-net

--- bot/bot.py
+++ bot/bot.py
@@ -13,7 +13,8 @@ import shutil
 
 from states import States
 from config import TOKEN
-from model_transfer_style import transfer_style
+from gatys_net import transfer_style as transfer_style_slow
+from msg_net import transfer_style as transfer_style_fast
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
@@ -34,17 +35,25 @@ def send_photo(chat_id, photo):
     print(r.status_code)
 
 
-def transfer_style_send_photo(file_content, file_style, file_output, num_steps, chat_id):
+def transfer_style_send_photo(model_name, file_content, file_style, file_output, chat_id):
     # print('Start')
-    # time.sleep(10)
+    # time.sleep(60)
     # print('Stop')
     # model start
-    transfer_style(file_content, file_style, file_output, num_steps=num_steps)
+    if model_name == '/gatys':
+        transfer_style_slow(file_content, file_style, file_output)
+    elif model_name == '/msg':
+        transfer_style_fast(file_content, file_style, file_output)
+    else:
+        print('Error! Model name wrong')
 
-    with open(file_output, 'rb') as photo:
-        send_photo(chat_id, photo)
+    if os.path.exists(file_output):
+        with open(file_output, 'rb') as photo:
+            send_photo(chat_id, photo)
 
-    shutil.rmtree(os.path.join(os.getcwd(), 'chat_id_%s' % chat_id))
+    folder_name = os.path.join(os.getcwd(), 'chat_id_%s' % chat_id)
+    if os.path.exists(folder_name):
+        shutil.rmtree(folder_name)
     calculations[chat_id] = False
 
 
@@ -59,11 +68,11 @@ class MyThread(threading.Thread):
 def process_queue():
     while True:
         try:
-            file_content, file_style, file_output, num_steps, chat_id = queue_transfer_style.get(block=False)
+            model_name, file_content, file_style, file_output, chat_id = queue_transfer_style.get(block=False)
         except queue.Empty:
             pass
         else:
-            transfer_style_send_photo(file_content, file_style, file_output, num_steps, chat_id)
+            transfer_style_send_photo(model_name, file_content, file_style, file_output, chat_id)
 
         time.sleep(1)
 
@@ -72,11 +81,29 @@ thread1 = MyThread()
 thread1.start()
 
 
-@dp.message_handler(commands=['start', 'help'], state='*')
+@dp.message_handler(commands=['start'], state='*')
 async def process_help_command(msg: types.Message):
-    text = ('Hi! I can transfer style from any image to your image.\n\n'
-            'You can control me by sending these commands:\n'
-            '/style_transfer - style transfer\n')
+    text = (
+        'Hi! I can transfer style from any image to your image.\n\n'
+        'You can control me by sending these commands:\n'
+        '/style_transfer - style transfer\n'
+        '/help - detailed instructions'
+    )
+    await bot.send_message(msg.from_user.id, text)
+
+
+@dp.message_handler(commands=['help'], state='*')
+async def process_help_command(msg: types.Message):
+    text = (
+        "To start the transfer of the style, you need:\n"
+        "1. to send the command /style_transfer\n"
+        "2. to send one image that needs to be processed\n"
+        "3. to send one image that contains the style\n"
+        '4. to choose a style transfer algorithm: '
+        'A Neural Algorithm of Artistic Style https://arxiv.org/abs/1508.06576 or '
+        'Multi-style Generative Network for Real-time Transfer https://arxiv.org/abs/1703.06953\n\n'
+        "Let's start!"
+    )
     await bot.send_message(msg.from_user.id, text)
 
 
@@ -86,11 +113,11 @@ async def style_transfer_command(msg: types.Message):
     if calculations[chat_id]:
         await bot.send_message(chat_id, "Wait for the end")
     else:
-        await bot.send_message(chat_id, "Send one jpg image to be processed")
+        await bot.send_message(chat_id, "Send one jpg image as photo to be processed")
         await States.STATE_AWAIT_ORIGINAL_IMAGE.set()
-        folder_name = 'chat_id_%s' % chat_id
+        folder_name = os.path.join(os.getcwd(), 'chat_id_%s' % chat_id)
         if os.path.exists(folder_name):
-            shutil.rmtree(os.path.join(os.getcwd(), folder_name))
+            shutil.rmtree(folder_name)
         os.mkdir(folder_name)
 
 
@@ -99,37 +126,39 @@ async def handle_get_original_photo(msg: types.Message):
     await States.STATE_AWAIT_STYLE_IMAGE.set()
     folder_name = 'chat_id_%s' % msg.from_user.id
     await msg.photo[-1].download(os.path.join(os.getcwd(), folder_name, 'original.jpg'))
-    await bot.send_message(msg.from_user.id, "Send one jpg image that contains the style")
+    await bot.send_message(msg.from_user.id, "Send one jpg image as photo that contains the style")
 
 
 @dp.message_handler(content_types=['photo'], state=States.STATE_AWAIT_STYLE_IMAGE)
 async def handle_get_transfer_strength(msg: types.Message):
-    await States.STATE_AWAIT_TRANSFER_STRENGTH.set()
+    await States.STATE_AWAIT_STYLE_METHOD.set()
     folder_name = 'chat_id_%s' % msg.from_user.id
     await msg.photo[-1].download(os.path.join(os.getcwd(), folder_name, 'style.jpg'))
-    await bot.send_message(msg.from_user.id, "Set the style transfer strength as a integer from 1 to 5")
+    text = (
+        'Set the style transfer method by sending one of two commands:\n'
+        '/gatys - A Neural Algorithm of Artistic Style (slow)\n'
+        '/msg - Multi-style Generative Network for Real-time Transfer (fast)'
+    )
+    await bot.send_message(msg.from_user.id, text)
 
 
-@dp.message_handler(content_types=['text'], state=States.STATE_AWAIT_TRANSFER_STRENGTH)
+@dp.message_handler(commands=['gatys', 'msg'], state=States.STATE_AWAIT_STYLE_METHOD)
 async def handle_get_style_photo(msg: types.Message):
-    if not msg.text in msg.text.strip() in list(map(str, range(1, 6))):
-        await bot.send_message(msg.from_user.id, "Input integer from 1 to 5, try again")
-        return
-
-    transfer_strength = int(msg.text)
-    num_steps = 100 * transfer_strength
-
     chat_id = msg.from_user.id
     folder_name = 'chat_id_%s' % chat_id
 
-    await bot.send_message(chat_id, "Style transfer in progress...")
-    file_content = os.path.join(os.getcwd(), folder_name, 'original.jpg')
-    file_style = os.path.join(os.getcwd(), folder_name, 'style.jpg')
-    file_output = os.path.join(os.getcwd(), folder_name, 'result.jpg')
+    if calculations[chat_id]:
+        await bot.send_message(chat_id, "Wait for the end")
+    else:
+        await bot.send_message(chat_id, "Style transfer in progress...")
+        file_content = os.path.join(os.getcwd(), folder_name, 'original.jpg')
+        file_style = os.path.join(os.getcwd(), folder_name, 'style.jpg')
+        file_output = os.path.join(os.getcwd(), folder_name, 'result.jpg')
 
-    # adding a style transfer job to the queue
-    queue_transfer_style.put((file_content, file_style, file_output, num_steps, chat_id))
-    calculations[chat_id] = True
+        # adding a style transfer job to the queue
+        model_name = msg.get_command().lower()
+        queue_transfer_style.put((model_name, file_content, file_style, file_output, chat_id))
+        calculations[chat_id] = True
 
 
 if __name__ == '__main__':
--- bot/states.py
+++ bot/states.py
@@ -4,4 +4,4 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 class States(StatesGroup):
     STATE_AWAIT_ORIGINAL_IMAGE = State()
     STATE_AWAIT_STYLE_IMAGE = State()
-    STATE_AWAIT_TRANSFER_STRENGTH = State()
+    STATE_AWAIT_STYLE_METHOD = State()
